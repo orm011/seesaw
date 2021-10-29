a4c8b28af911f1614b3c384d27f4d877a396ad29
Oscar Moll <orm@csail.mit.edu>
2021-10-29 11:40:04 -0400

    simplify data scripts. move logic to server script.  scripts work in spc env.

--- init_data_actors.py
+++ init_data_actors.py
@@ -12,62 +12,12 @@ import pyroaring as pr
 import time
 import functools
 
+import seesaw
 from seesaw import *
-# from .cross_modal_db import *
-# from .embeddings import *
-# from .dataset_tools import *
-# from .vloop_dataset_loaders import *
-# from .search_loop_models import *
-# from .search_loop_tools import *
-# from .ui import widgets
-# from .vls_benchmark_tools import vls_init_logger
-
-class InteractiveQueryRemote(object):
-    def __init__(self, dbactor, batch_size: int):
-        self.dbactor = dbactor
-        self.seen = pr.BitMap()
-        self.query_history = []
-        self.acc_idxs = []
-        self.batch_size = batch_size
-
-    def query_stateful(self, *args, **kwargs):
-        '''
-        :param kwargs: forwards arguments to db query method but
-         also keeping track of already seen results. also
-         keeps track of query history.
-        :return:
-        '''
-        batch_size = kwargs.get('batch_size',self.batch_size)
-        if 'batch_size' in kwargs:
-            del kwargs['batch_size']
-            
-        idxref = self.dbactor.query.remote(*args, topk=batch_size, **kwargs, exclude=self.seen)
-        idxs, _ = ray.get(idxref)
-        self.query_history.append((args, kwargs))
-        self.seen.update(idxs)
-        self.acc_idxs.append(idxs)
-        return idxs
-    
-    def repeat_last(self):
-        '''
-        :return: continues the search from last query, effectively paging through results
-        '''
-        assert self.query_history != []
-        args, kwargs = self.query_history[-1]
-        return self.query_stateful(*args, **kwargs)
-    
-class BoxFeedbackQueryRemote(InteractiveQueryRemote):
-    def __init__(self, dbactor, batch_size, auto_fill_df=None):
-        super().__init__(dbactor, batch_size)
-        self.label_db = {}
-        # self.rois = [[]]
-        # self.augmented_rois = [[]]
-        # self.roi_vecs = [np.zeros((0, dbactor..embedded.shape[1]))]
-        self.auto_fill_df = auto_fill_df
 
 ## make the embedding model itself a separate actor
 # pass handle all the way down.
-# I want calls to be synchronous
+# I want calls to be synchronous“
 class ModelService(XEmbedding):
     def __init__(self, model_ref):
         self.model_ref = model_ref
@@ -82,78 +32,48 @@ class ModelService(XEmbedding):
         return ray.get(self.model_ref.from_raw.remote(*args, **kwargs))
 
 class DB(object):
-    def __init__(self, dataset_loader, model_handle, dbsample, valsample):
-        # NB: in reality the n_px below may have been different for the embedding path we pass,
-        # fix before using query by example. 
-        ev0 = dataset_loader(model_handle)
+    evref : ray.ObjectRef
+    def __init__(self, dataset_loader, dbsample=None):
+        print('loading ev...') 
+        ev = dataset_loader()
+        print('loaded ev...now putting it into shared store')
+        self.ev_ref = ray.put(ev)
+        print('put ev into store')
+        del ev # don't keep local ref
 
-        if dbsample is not None:
-            ev = extract_subset(ev0, idxsample=np.sort(dbsample))
-        else:
-            ev = ev0
+        assert dbsample is None
 
-        if valsample is not None:
-            val = extract_subset(ev0, idxsample=np.sort(valsample))
-            self.val_vec = val.db.embedded
-            self.val_gt = val.query_ground_truth
-        else:
-            self.val_vec = None
-            self.val_gt = None
-
-
-        # self.qgt = ray.put(ev.query_ground_truth) # make this global so that we don't copy it for every worker
-        # self.box_data = ray.put(ev.box_data) # same as above
-        qgt = ev.query_ground_truth
-        self.positive_sets = {k:pr.BitMap(v[v == 1].index) for k,v in qgt.items() }
-        self.negative_sets = {k:pr.BitMap(v[v == 0].index) for k,v in qgt.items() }
-
-        vec_meta = ev.fine_grained_meta
-        vecs = ev.fine_grained_embedding
-        #index_path = './data/bdd_10k_allgrains_index.ann'
-        index_path = None
-        self.hdb = AugmentedDB(raw_dataset=ev.image_dataset, embedding=ev.embedding, 
-            embedded_dataset=vecs, vector_meta=vec_meta, index_path=index_path)
-
+        ## do this at client side
+        # if dbsample is not None:
+        #     ev = extract_subset(ev0, idxsample=np.sort(dbsample))
         #self.hdb = EmbeddingDB(ev.image_dataset,ev.embedding,ev.embedded_dataset)
-        self.ev = ev
-        print('inited dbactor') 
+        print('inited db...') 
     
-    def get_subsets(self, categories):
-        ans = {}
-        for cat in categories:
-            ans[cat] = {'pos':self.positive_sets[cat], 
-            'neg':self.negative_sets[cat]}
+    def ready(self):
+        return True
 
-        return ans
+    def get_ev(self):
+        return self.ev_ref
 
-    def extract_subset(self, idxs, categories, boxes):
-        ## seems like raw subset forces copy of everything to store due to numpy refs
-        return extract_subset(self.ev, idxsample=idxs, categories=categories, boxes=boxes).copy()
+    def get_image_paths(self, idxs):
+        paths = self.ev.image_dataset.paths[idxs]
+        from_root = self.ev.image_dataset.root + '/' + paths
+        return from_root
 
-    def get_urls(self, idxs):
-        return self.hdb.raw.get_urls(idxs)
-
-    def get_val(self):
-        return (self.val_vec, self.val_gt)
-
-    def get_qgt(self):
-        return self.qgt
-        #return self.ev.query_ground_truth
-        
     def embed_raw(self, data):
         return self.hdb.embedding.from_raw(data)
     
-    def get_boxes(self, category):
-        c = category
-        assert category in self.ev.query_ground_truth.columns
-        box_cats = self.ev.box_data.category.unique()         
-        if c not in box_cats: # NB box bounds only work for bdd... fix before using example
-            dbidxs = np.where(self.ev.query_ground_truth[c] > 0)[0]
-            boxdf = pd.DataFrame({'dbidx':dbidxs})
-            boxes = boxdf.assign(x1=0, y1=0, x2=1280, y2=720, category=c)
-        else:
-            boxes = self.ev.box_data[self.ev.box_data.category == c]
-        return boxes
+    # def get_boxes(self, category):
+    #     c = category
+    #     assert category in self.ev.query_ground_truth.columns
+    #     box_cats = self.ev.box_data.category.unique()         
+    #     if c not in box_cats: # NB box bounds only work for bdd... fix before using example
+    #         dbidxs = np.where(self.ev.query_ground_truth[c] > 0)[0]
+    #         boxdf = pd.DataFrame({'dbidx':dbidxs})
+    #         boxes = boxdf.assign(x1=0, y1=0, x2=1280, y2=720, category=c)
+    #     else:
+    #         boxes = self.ev.box_data[self.ev.box_data.category == c]
+    #     return boxes
     
     def get_vectors(self, idxs=None):
         if idxs is None:
@@ -166,151 +86,33 @@ class DB(object):
         return self.hdb.query(topk=topk, mode=mode, cluster_id=cluster_id, 
                               vector=vector, exclude=exclude, return_scores=return_scores)
 
-DBActor = ray.remote(DB)
+RemoteDB = ray.remote(DB)
 
-def get_panel_data_remote(q, label_db, next_idxs):
-    reslabs = []
-    for (i,dbidx) in enumerate(next_idxs):
-        boxes = copy.deepcopy(label_db.get(dbidx, None))
-        reslabs.append({'value': -1 if boxes is None else 1 if len(boxes) > 0 else 0, 
-                        'id': i, 'dbidx': int(dbidx), 'boxes': boxes})
-    urls = ray.get(q.dbactor.get_urls.remote(next_idxs))
-    pdata = {
-        'image_urls': urls,
-        'ldata': reslabs,
-    }
-    return pdata
-
-def make_image_panel_remote(bfq, idxbatch):
-    dat = get_panel_data_remote(bfq, bfq.label_db, idxbatch)
-
-    ldata = dat['ldata']
-    if bfq.auto_fill_df is not None:
-        gt_ldata = auto_fill_boxes(bfq.auto_fill_df, ldata)
-        ## only use boxes for things we have not added ourselves...
-        ## (ie don't overwrite db)
-        for rdb, rref in zip(ldata, gt_ldata):
-            if rdb['dbidx'] not in bfq.label_db:
-                rdb['boxes'] = rref['boxes']
-
-    pn = widgets.MImageGallery(**dat)
-    return pn
-
-def fit(*, mod, X, y, batch_size, valX=None, valy=None, logger=None,  max_epochs=6, gpus=0, precision=32):
-    class CustomInterrupt(pl.callbacks.Callback):
-        def on_keyboard_interrupt(self, trainer, pl_module):
-            raise InterruptedError('custom')
-
-    class CustomTqdm(pl.callbacks.progress.ProgressBar):
-        def init_train_tqdm(self):
-            """ Override this to customize the tqdm bar for training. """
-            bar = tqdm(
-                desc='Training',
-                initial=self.train_batch_idx,
-                position=(2 * self.process_position),
-                disable=self.is_disabled,
-                leave=False,
-                dynamic_ncols=True,
-                file=sys.stdout,
-                smoothing=0,
-                miniters=40,
-            )
-            return bar
-    
-    if not torch.is_tensor(X):
-        X = torch.from_numpy(X)
-    
-    train_ds = TensorDataset(X,torch.from_numpy(y))
-    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=0)
-
-    if valX is not None:
-        if not torch.is_tensor(valX):
-            valX = torch.from_numpy(valX)
-        val_ds = TensorDataset(valX, torch.from_numpy(valy))
-        es = [pl.callbacks.early_stopping.EarlyStopping(monitor='AP/val', mode='max', patience=3)]
-        val_loader = DataLoader(val_ds, batch_size=2000, shuffle=False, num_workers=0)
-    else:
-        val_loader = None
-        es = []
-
-    trainer = pl.Trainer(logger=None, 
-                         gpus=gpus, precision=precision, max_epochs=max_epochs,
-                         callbacks =[],
-                        #  callbacks=es + [ #CustomInterrupt(),  # CustomTqdm()
-                        #  ], 
-                         checkpoint_callback=False,
-                         progress_bar_refresh_rate=0, #=10
-                        )
-    trainer.fit(mod, train_loader, val_loader)
-
-
-def update_vector(Xt, yt, init_vec, minibatch_size):
-    p = yt.sum()/yt.shape[0]
-    w = np.clip((1-p)/p, .1, 10.)
-    lr = PTLogisiticRegression(Xt.shape[1], learning_rate=.0003, C=0, positive_weight=w)
-
-    if init_vec is not None:
-        iv = torch.from_numpy(init_vec)
-        iv = iv / iv.norm()
-        lr.linear.weight.data = iv.type(lr.linear.weight.dtype)
-
-    fit(mod=lr, X=Xt.astype('float32'), y=yt.astype('float'), batch_size=minibatch_size)
-    tvec = lr.linear.weight.detach().numpy().reshape(1,-1)   
-    return tvec
-
-
-#lvishot = functools.partial(lvis_category, category='hot-air balloon')
-
-default_actors = {
-    'lvis':lambda m,ng: ray.remote(DB).options(name='lvis_db', lifetime="detached", num_gpus=ng, num_cpus=2).remote(dataset_loader=lvis_full,
-                                model_handle=m,  
-                                dbsample = None,
-                                #dbsample=np.sort(np.load('./data/coco_30k_idxs.npy')[:10000]),
-                                valsample=None), #np.load('./data/coco_30k_idxs.npy')[10000:20000]),
-    'coco':lambda m,ng: ray.remote(DB).options(name='coco_db', lifetime="detached", num_gpus=ng, num_cpus=.1).remote(dataset_loader=coco_full, 
-                                model_handle=m,
-                                dbsample=np.load('./data/coco_30k_idxs.npy')[:10000],
-                                valsample=np.load('./data/coco_30k_idxs.npy')[10000:20000]),
-    'mini_coco':lambda m,ng: ray.remote(DB).options(name='mini_coco_db', lifetime="detached", num_gpus=ng, num_cpus=.1).remote(dataset_loader=mini_coco, 
-                                model_handle=m,
-                                dbsample=None, #np.load('./data/coco_30k_idxs.npy')[:10000],
-                                valsample=None), # np.load('./data/coco_30k_idxs.npy')[10000:20000]),
-    'dota':lambda m,ng: ray.remote(DB).options(name='dota_db', lifetime="detached", num_gpus=ng, num_cpus=.1).remote(dataset_loader=dota1_full,
-                                model_handle=m,
-                                dbsample=np.load('./data/dota_idxs.npy')[:1000], # size is 1860 or so.
-                                valsample=None,#np.load('./data/dota_idxs.npy')[1000:]
-                                ),
-    'ava': lambda m,ng: ray.remote(DB).options(name='ava_db', num_gpus=ng, num_cpus=.1).remote(dataset_loader=ava22, 
-                                model_handle=m,
-                                dbsample=np.load('./data/ava_randidx.npy')[:10000],
-                                valsample=np.load('./data/ava_randidx.npy')[10000:20000]), 
-    'bdd': lambda m,ng: ray.remote(DB).options(name='bdd_db', lifetime="detached", num_gpus=ng, num_cpus=.1).remote(dataset_loader=bdd_full, 
-                                model_handle=m,
-                                dbsample=np.sort(np.load('./data/bdd_20kidxs.npy')[:10000]),
-                                valsample=np.load('./data/bdd_20kidxs.npy')[10000:20000]),
-    'objectnet': lambda m,ng: ray.remote(DB).options(name='objectnet_db', lifetime="detached", num_gpus=ng, num_cpus=.1).remote(dataset_loader=objectnet_cropped, 
-                                model_handle=m,
-                                dbsample=np.load('./data/object_random_idx.npy')[:10000],
-                                valsample=np.load('./data/object_random_idx.npy')[10000:20000])
-}
-
+import argparse
 
 if __name__ == '__main__':    
     ray.init('auto', namespace='seesaw')
 
     model_actor = ray.get_actor('clip')
-    # ray.remote(CLIPWrapper).options(name='clip', lifetime='detached', num_gpus=num_gpus, num_cpus=.1).remote(device=device)
-    model_service = ModelService(model_actor)
-    print('inited model')
+    xclip = ModelService(model_actor)
 
-    actors = []
-    for (k,v) in default_actors.items():
-        if k in ['mini_coco']:
-            print('init ', k)
-            actors.append(v(model_service, 0))
+    gdm = GlobalDataManager('/home/gridsan/omoll/seesaw_root/data')
+    ds_names = ['objectnet']#, 'bdd', 'coco', 'dota', 'lvis']
 
+    dbs = []
+    handles = []
+    for k in ds_names:
+        def loader():
+            return load_ev(gdm=gdm, dsname=k, xclip=xclip, load_ground_truth=False, load_coarse=True)
 
-    ## I find that if the script ends immediately, the actor dies even in detached mode, so use this
-    ## (can still run in background)
-    time.sleep(20)
-    print('db loaded')    
+        dbactor = RemoteDB.options(name=k).remote(dataset_loader=loader, dbsample=None)
+        dbs.append(dbactor)
+        handles.append(dbactor.ready.remote())
+
+    handles.append(model_actor.ready.remote())
+    ray.get(handles)
+    print('db loaded and model ready')
+
+    while input() != 'exit':
+        pass
+    print('exiting...')
--- session_server.py
+++ session_server.py
@@ -1,36 +1,186 @@
+import os
+os.environ['OMP_NUM_THREADS'] = f'{os.cpu_count()//2}'
+
 import flask
 from flask import Flask, request
 import ray
+ray.init('auto', namespace='seesaw')
 
-
+import seesaw
 from seesaw import *
-# from .search_loop_models import *
-# from .search_loop_tools import *
-# from .vloop_dataset_loaders import *
-# from .cross_modal_db import *
-# from .embedding_plot import *
-# from .embeddings import *
-from init_data_actors import BoxFeedbackQueryRemote, get_panel_data_remote, update_vector
+from init_data_actors import DB, RemoteDB, ModelService
 
 
-ray.init('auto', 
-    #ignore_reinit_error=True,
-        namespace='seesaw')
-default_dataset = 'mini_coco'
-datasets = ['mini_coco',] #'lvis',  ]#['coco', 'ava', 'bdd', 'dota', 'objectnet', 'lvis']
-dbactors = dict([(name,ray.get_actor('{}_db'.format(name))) for name in datasets])
+gdm = GlobalDataManager('/home/gridsan/omoll/seesaw_root/data')
+
+datasets = ['objectnet']#,[ 'dota', 'lvis','coco', 'bdd']
+default_dataset = datasets[0]
+dbactors = dict([(name,ray.get_actor(name)) for name in datasets])
+
+class InteractiveQueryRemote(object):
+    def __init__(self, dbactor, batch_size: int):
+        self.dbactor = dbactor
+        self.seen = pr.BitMap()
+        self.query_history = []
+        self.acc_idxs = []
+        self.batch_size = batch_size
+
+    def query_stateful(self, *args, **kwargs):
+        '''
+        :param kwargs: forwards arguments to db query method but
+         also keeping track of already seen results. also
+         keeps track of query history.
+        :return:
+        '''
+        batch_size = kwargs.get('batch_size',self.batch_size)
+        if 'batch_size' in kwargs:
+            del kwargs['batch_size']
+            
+        idxref = self.dbactor.query.remote(*args, topk=batch_size, **kwargs, exclude=self.seen)
+        idxs, _ = ray.get(idxref)
+        self.query_history.append((args, kwargs))
+        self.seen.update(idxs)
+        self.acc_idxs.append(idxs)
+        return idxs
+    
+    def repeat_last(self):
+        '''
+        :return: continues the search from last query, effectively paging through results
+        '''
+        assert self.query_history != []
+        args, kwargs = self.query_history[-1]
+        return self.query_stateful(*args, **kwargs)
+    
+class BoxFeedbackQueryRemote(InteractiveQueryRemote):
+    def __init__(self, dbactor, batch_size, auto_fill_df=None):
+        super().__init__(dbactor, batch_size)
+        self.label_db = {}
+        # self.rois = [[]]
+        # self.augmented_rois = [[]]
+        # self.roi_vecs = [np.zeros((0, dbactor..embedded.shape[1]))]
+        self.auto_fill_df = auto_fill_df
+
+
+
+def get_panel_data_remote(q, ev, label_db, next_idxs):
+    reslabs = []
+    for (i,dbidx) in enumerate(next_idxs):
+        boxes = copy.deepcopy(label_db.get(dbidx, None))
+        reslabs.append({'value': -1 if boxes is None else 1 if len(boxes) > 0 else 0, 
+                        'id': i, 'dbidx': int(dbidx), 'boxes': boxes})
+    urls = get_image_paths(ev, next_idxs)
+    # q.db.get_image_paths(next_idxs)
+    pdata = {
+        'image_urls': urls,
+        'ldata': reslabs,
+    }
+    return pdata
+
+def make_image_panel_remote(bfq, idxbatch):
+    dat = get_panel_data_remote(bfq, bfq.label_db, idxbatch)
+
+    ldata = dat['ldata']
+    if bfq.auto_fill_df is not None:
+        gt_ldata = auto_fill_boxes(bfq.auto_fill_df, ldata)
+        ## only use boxes for things we have not added ourselves...
+        ## (ie don't overwrite db)
+        for rdb, rref in zip(ldata, gt_ldata):
+            if rdb['dbidx'] not in bfq.label_db:
+                rdb['boxes'] = rref['boxes']
+
+    pn = widgets.MImageGallery(**dat)
+    return pn
+
+def fit(*, mod, X, y, batch_size, valX=None, valy=None, logger=None,  max_epochs=6, gpus=0, precision=32):
+    class CustomInterrupt(pl.callbacks.Callback):
+        def on_keyboard_interrupt(self, trainer, pl_module):
+            raise InterruptedError('custom')
+
+    class CustomTqdm(pl.callbacks.progress.ProgressBar):
+        def init_train_tqdm(self):
+            """ Override this to customize the tqdm bar for training. """
+            bar = tqdm(
+                desc='Training',
+                initial=self.train_batch_idx,
+                position=(2 * self.process_position),
+                disable=self.is_disabled,
+                leave=False,
+                dynamic_ncols=True,
+                file=sys.stdout,
+                smoothing=0,
+                miniters=40,
+            )
+            return bar
+    
+    if not torch.is_tensor(X):
+        X = torch.from_numpy(X)
+    
+    train_ds = TensorDataset(X,torch.from_numpy(y))
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=0)
+
+    if valX is not None:
+        if not torch.is_tensor(valX):
+            valX = torch.from_numpy(valX)
+        val_ds = TensorDataset(valX, torch.from_numpy(valy))
+        es = [pl.callbacks.early_stopping.EarlyStopping(monitor='AP/val', mode='max', patience=3)]
+        val_loader = DataLoader(val_ds, batch_size=2000, shuffle=False, num_workers=0)
+    else:
+        val_loader = None
+        es = []
+
+    trainer = pl.Trainer(logger=None, 
+                         gpus=gpus, precision=precision, max_epochs=max_epochs,
+                         callbacks =[],
+                        #  callbacks=es + [ #CustomInterrupt(),  # CustomTqdm()
+                        #  ], 
+                         checkpoint_callback=False,
+                         progress_bar_refresh_rate=0, #=10
+                        )
+    trainer.fit(mod, train_loader, val_loader)
+
+
+def update_vector(Xt, yt, init_vec, minibatch_size):
+    p = yt.sum()/yt.shape[0]
+    w = np.clip((1-p)/p, .1, 10.)
+    lr = PTLogisiticRegression(Xt.shape[1], learning_rate=.0003, C=0, positive_weight=w)
+
+    if init_vec is not None:
+        iv = torch.from_numpy(init_vec)
+        iv = iv / iv.norm()
+        lr.linear.weight.data = iv.type(lr.linear.weight.dtype)
+
+    fit(mod=lr, X=Xt.astype('float32'), y=yt.astype('float'), batch_size=minibatch_size)
+    tvec = lr.linear.weight.detach().numpy().reshape(1,-1)   
+    return tvec
 
 
 class SessionState(object):
-    def __init__(self, dataset_name, gt_class=None):
-        self.current_dataset = dataset_name
-        self.dbactor = dbactors[self.current_dataset]
-        if gt_class is not None:
-            self.box_data = ray.get(self.dbactor.get_boxes.remote())
-        else:
-            self.box_data = None
+    ev : EvDataset
+    hdb : AugmentedDB
 
-        self.bfq = BoxFeedbackQueryRemote(self.dbactor, batch_size=10, auto_fill_df=self.box_data)
+    def __init__(self, dataset_name):
+        self.current_dataset = dataset_name
+        dbactor = dbactors[self.current_dataset]
+        evhandle = ray.get(dbactor.get_ev.remote())
+        print('getting ev state')
+        ev = ray.get(evhandle)
+        self.ev = ev
+        print('done getting state')
+
+        ## using coarse right now
+        self.hdb = EmbeddingDB(raw_dataset=ev.image_dataset, embedding=ev.embedding,embedded_dataset=ev.embedded_dataset)
+        
+        # AugmentedDB(raw_dataset=ev.image_dataset, embedding=ev.embedding, 
+        #     embedded_dataset=ev.fine_grained_embedding, vector_meta=ev.fine_grained_meta,
+        #     vec_index=ev.vec_index)
+
+        # if gt_class is not None:
+        #     self.box_data = ray.get(self.dbactor.get_boxes.remote())
+        # else:
+        #     self.box_data = None
+        self.box_data = None
+        self.bfq = BoxFeedbackQuery(self.hdb, batch_size=10, auto_fill_df=self.box_data)
+        # self.bfq = BoxFeedbackQueryRemote(self.dbactor, batch_size=10, auto_fill_df=self.box_data)
         self.init_vec = None
         self.acc_indices = np.array([])
 
@@ -38,22 +188,26 @@ class SessionState(object):
         if dataset_name is not None:
             self.current_dataset = dataset_name
         self.dbactor = dbactors[self.current_dataset]
-        self.bfq = BoxFeedbackQueryRemote(self.dbactor, batch_size=5, auto_fill_df=self.box_data)
+        self.bfq = BoxFeedbackQuery(self.hdb, batch_size=5, auto_fill_df=self.box_data)
         self.init_vec = None
         self.acc_indices = np.array([])
 
     def get_state(self):
-        dat = get_panel_data_remote(self.bfq,  self.bfq.label_db, self.acc_indices)
+        dat = get_panel_data_remote(self.bfq, self.ev,  self.bfq.label_db, self.acc_indices)
         dat['datasets'] = datasets
         dat['current_dataset'] = self.current_dataset
         return dat
 
     def get_latest(self):
-        dat = get_panel_data_remote(self.bfq,  self.bfq.label_db, self.bfq.acc_idxs[-1])
+        dat = get_panel_data_remote(self.bfq,  self.ev, self.bfq.label_db, self.bfq.acc_idxs[-1])
         return dat
 
+
+state : SessionState
 state = SessionState(default_dataset)
 
+def get_image_paths(ev, idxs):
+    return [ f'/data/{state.current_dataset}/images/{ev.image_dataset.paths[int(i)]}' for i in idxs]
 
 print('inited state... about to create app')
 app = Flask(__name__)
@@ -78,7 +232,7 @@ def getstate():
 @app.route('/text', methods=['POST'])
 def text():
     query = request.args.get('key', '')
-    state.init_vec = ray.get(state.dbactor.embed_raw.remote(query))
+    state.init_vec = state.ev.embedding.from_string(string=query)
     return next()
 
 @app.route('/search_hybrid', methods=['POST'])
@@ -124,19 +278,20 @@ def next():
     if mask.sum() > 0:
         labelled_results = acc_results[mask]
         labelled_indices = state.acc_indices[mask]
-        Xt = ray.get(state.dbactor.get_vectors.remote(labelled_indices))
+        Xt = state.ev.embedded_dataset[labelled_indices] #ray.get(state.db.get_vectors.remote(labelled_indices))
         yt = labelled_results.astype('float')
 
         if (yt == 0).any() and (yt == 1).any():
             tvec = update_vector(Xt,yt, state.init_vec, minibatch_size=1)
 
-    idxbatch = state.bfq.query_stateful(mode='dot', vector=tvec, batch_size=5)
-    state.acc_indices = np.concatenate([state.acc_indices, idxbatch])
+    idxbatch,_ = state.bfq.query_stateful(mode='dot', vector=tvec, batch_size=5)
+    state.acc_indices = np.concatenate([state.acc_indices, idxbatch.reshape(-1)])
     dat = state.get_latest()
     # acc_results = np.array([litem['value'] for litem in dat['ldata']])
     # print('after idx', state.acc_indices)
     # print('after labels', acc_results)
     return flask.jsonify(**dat)
 
+print('done')
 if __name__ == '__main__':
     pass
