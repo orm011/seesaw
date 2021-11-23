db87c3241343f111fb8234c8f7c0520608199117
Oscar Moll <orm@csail.mit.edu>
2021-11-23 14:41:23 -0500

    remove outdated init scripts, stale dockerfile for now

--- run_notebook.bash
+++
@@ -1,7 +0,0 @@
-jupyter notebook --allow-root --no-browser \
-     --ip=0.0.0.0 --port=8888 \
-     --NotebookApp.token='' --NotebookApp.password=''\
-     --MultiKernelManager.default_kernel_name='seesaw'\
-     --KernelSpecManager.whitelist 'seesaw' \
-     --NotebookApp.notebook_dir='/workdir/notebooks'\
-     --KernelSpecManager.ensure_native_kernel=False
