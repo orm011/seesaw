b76483d3b105258376742da322e91e27828ea2a8
Oscar Moll <orm@csail.mit.edu>
2022-01-13 13:55:23 -0500

    coarse index works also

--- seesaw/coarse_index.py
+++ seesaw/coarse_index.py
@@ -1,24 +1,17 @@
-import torchvision
-import torch
 import pandas as pd
 import os
 import numpy as np
-import PIL.Image
 import torch.nn as nn
 import torch.nn.functional as F
-import sklearn
 import torchvision.transforms as T
 import pyroaring as pr
 
-from collections import deque, defaultdict
 import sklearn
-from sklearn.neighbors import NearestNeighbors, KNeighborsRegressor
-from sklearn.linear_model import LogisticRegression
-import typing
 import torch.utils.data
 from .embeddings import XEmbedding
 from .query_interface import *
 import torch
+from .dataset_manager import GlobalDataManager
 
 class CoarseIndex(AccessMethod):
     """Structure holding a dataset,
@@ -27,14 +20,35 @@ class CoarseIndex(AccessMethod):
     """
     def __init__(self, images : torch.utils.data.Dataset,
                  embedding : XEmbedding,
-                 vectors : np.ndarray):
+                 vectors : np.ndarray, 
+                 vector_meta : pd.DataFrame):
         self.images = images
         self.embedding = embedding
-        all_indices = pr.BitMap()
-        all_indices.add_range(0, len(self.images))
-        self.all_indices = pr.FrozenBitMap(all_indices)
         self.vectors = vectors
-        assert len(self.images) >= self.vectors.shape[0]
+        self.vector_meta = vector_meta
+
+        self.all_indices = pr.FrozenBitMap(self.vector_meta.dbidx.values)
+        #assert len(self.images) >= self.vectors.shape[0]
+    
+    def string2vec(self, string : str) -> np.ndarray:
+        init_vec = self.embedding.from_string(string=string)
+        init_vec = init_vec/np.linalg.norm(init_vec)
+        return init_vec
+
+    @staticmethod
+    def from_path(gdm : GlobalDataManager, dataset_name : str, index_subpath : str, model_name :str):
+        dm = gdm.get_dataset(dataset_name)
+        images = dm.get_pytorch_dataset()
+        embedding = gdm.get_model_actor(model_name)
+        
+        coarse_meta_path= f'{gdm.root}/{index_subpath}/vectors.coarse.cached'
+
+        assert os.path.exists(coarse_meta_path)
+        coarse_df = pd.read_parquet(coarse_meta_path)
+        assert coarse_df.dbidx.is_monotonic_increasing, 'sanity check'
+        embedded_dataset = coarse_df['vectors'].values.to_numpy()
+        vector_meta = coarse_df.drop('vectors', axis=1)
+        return CoarseIndex(images=images, embedding=embedding, vectors=embedded_dataset, vector_meta=vector_meta)
     
     def query(self, *, topk, mode, vector=None, exclude=None, startk=None):
         if exclude is None:
