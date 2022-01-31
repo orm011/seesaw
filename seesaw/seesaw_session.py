d44c95554436854f1874b86e7e1f5e50112980d1
Oscar Moll <orm@csail.mit.edu>
2022-01-31 17:31:12 -0500

    checkpoint

--- seesaw/seesaw_session.py
+++ seesaw/seesaw_session.py
@@ -68,7 +68,33 @@ class SeesawLoop:
 
         if self.params.interactive == 'textual':
           self.state.string_encoder = StringEncoder('cpu')
-          self.state.updater = Updater(self.state.string_encoder, lr=.0001)
+
+          config = {'batch_size': 64,
+                  'logit_scale_init': 3.7,
+                  'opt_config': {'logit_scale': {'lr': 0.0001415583047102676,
+                    'weight_decay': 0.0017007389655182095},
+                    'transformer': None,
+                    'transformer.resblocks.0.ln_': {'lr': 0.0007435612322566577,
+                    'weight_decay': 1.5959136512232553e-05},
+                    'transformer.resblocks.11': {'lr': 0.0001298217305130271,
+                    'weight_decay': 0.015548602355938877},
+                    'transformer.resblocks.11.mlp': {'lr': 3.258792283209162e-07,
+                    'weight_decay': 0.001607367028678558},
+                    'transformer.resblocks.11.ln_2': None,
+                    'ln_final': {'lr': 0.007707377565843718,
+                    'weight_decay': 0.0},
+                    'text_projection': {'lr': 2.581683501371101e-05, 'weight_decay': 0.0},
+                    'positional_embedding':None,
+                    'token_embedding':None,
+                    'visual': None,
+                    'positiional_embedding':None},
+                  'num_warmup_steps': 20,
+                  'num_workers': 20,
+                  'test_size': 1000,
+                  'margin':.3,
+                  'rounds':10,
+                  'val_batch_size': 500}
+          self.state.updater = Updater(self.state.string_encoder, config)
 
     def next_batch(self):
         """
@@ -124,8 +150,8 @@ class SeesawLoop:
 
           all_vecs = np.concatenate(vecs)
           all_strs = np.concatenate(strs)
-          s.updater.update(all_vecs, all_strs, s.curr_str)
-          print('done with update')
+          losses = s.updater.update(all_vecs, all_strs, s.curr_str)
+          print('done with update', losses)
         else:
             Xt,yt = self.q.getXy()
             lp['n_posvecs'] = (yt == 1).sum()#.shape[0]
