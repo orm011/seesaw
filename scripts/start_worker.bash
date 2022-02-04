6a69b367677d5cf6f57a410496f8245b32d32abd
Oscar Moll <orm@csail.mit.edu>
2022-02-04 10:15:50 -0500

    workers set cpu num based on slurm allocation

--- scripts/start_worker.bash
+++ scripts/start_worker.bash
@@ -8,18 +8,14 @@ TMPNAME=/state/partition1/user/$USER/raytmp/
 OBJ_MEM_GB=64 # cpu work nodes have around 94, gpu nodes have 79  
 OBJ_MEM_BYTES=$(( $OBJ_MEM_GB*(2**30) ))
 
-#export PM2_HOME=/state/partition1/$USER/omoll/pm2_home
+COMMON_ARGS="--temp-dir=$TMPNAME  --object-store-memory=$OBJ_MEM_BYTES --num-cpus=$((2*SLURM_CPUS_ON_NODE))"
 
 if [[ $1 == "--head" ]];
 then
     echo $HOSTNAME:6379 > /home/gridsan/$USER/init_spc.head_node
-    ray start --head --temp-dir=$TMPNAME --object-store-memory=$OBJ_MEM_BYTES
-else
-    # assumes this is the only app we run with pm2
-    # install pm2 with npm: npm install pm2 -g
+    ray start $COMMON_ARGS --head
+else 
     HEAD_ADDRESS=`cat /home/gridsan/$USER/init_spc.head_node`
-    RAY_CMD="ray start --block --address=$HEAD_ADDRESS --redis-password=5241590000000000 --temp-dir=$TMPNAME  --object-store-memory=$OBJ_MEM_BYTES"
-    pkill -9 PM2
-    pkill -9 ray
-    pm2 start "$RAY_CMD" --no-daemon
+    ray start $COMMON_ARGS --address=$HEAD_ADDRESS --redis-password=5241590000000000 
+    sleep infinity # allows me to restart this ray node from elsewhre
 fi
