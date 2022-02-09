3c508a15459d97467bfe1e74c7d629e077e54267
Oscar Moll <orm@csail.mit.edu>
2022-02-09 16:15:00 -0500

    pass flags to add nodes

--- scripts/add_nodes.bash
+++ scripts/add_nodes.bash
@@ -1,7 +1,8 @@
 N=$1
 for i in `seq $N`; do
     #LLsub /home/gridsan/omoll/seesaw/scripts/start_worker.bash -g volta:1 -s 20 &
-    LLsub /home/gridsan/omoll/seesaw/scripts/start_worker.bash  -s 48  &
+    LLsub /home/gridsan/omoll/seesaw/scripts/start_worker.bash $2 &
+    #LLsub /home/gridsan/omoll/seesaw/scripts/start_worker.bash  -s 48  &
 done
 
 wait
