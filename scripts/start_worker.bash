fe4c982d2f45ae0b77d14ea4b19d0d450382fb54
Oscar Moll <orm@csail.mit.edu>
2022-03-18 23:13:19 -0400

    update scripts

--- scripts/start_worker.bash
+++ scripts/start_worker.bash
@@ -1,8 +1,6 @@
 #! /bin/bash
 set -x
 
-cd /home/gridsan/$USER/seesaw/scripts/
-
 TMPNAME=/state/partition1/user/$USER/raytmp/
 
 OBJ_MEM_GB=64 # cpu work nodes have around 94, gpu nodes have 79  
@@ -12,10 +10,10 @@ COMMON_ARGS="--temp-dir=$TMPNAME  --object-store-memory=$OBJ_MEM_BYTES --num-cpu
 
 if [[ $1 == "--head" ]];
 then
-    echo $HOSTNAME:6379 > /home/gridsan/$USER/init_spc.head_node
+    echo $HOSTNAME:6379 > $HOME/init_spc.head_node
     ray start $COMMON_ARGS --head
 else 
-    HEAD_ADDRESS=`cat /home/gridsan/$USER/init_spc.head_node`
+    HEAD_ADDRESS=`cat $HOME/init_spc.head_node`
     ray start $COMMON_ARGS --address=$HEAD_ADDRESS --redis-password=5241590000000000 
     sleep infinity # allows me to restart this ray node from elsewhre
 fi
