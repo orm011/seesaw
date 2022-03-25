0c1b9a627b4898f47b1a92379ae791b5a6159f7e
Oscar Moll <orm@csail.mit.edu>
2022-03-25 14:30:36 -0400

    move css import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
 import App from './SeesawApp.vue'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app')
