1aa5bf769961b4bb80d425275fb968a359d2af49
Oscar Moll <orm@csail.mit.edu>
2022-04-09 12:33:15 -0400

    split seesaw web into per-session state machine and multiplexer api

--- seesaw/seesaw_web.py
+++ seesaw/seesaw_web.py
@@ -4,7 +4,7 @@ from typing import Optional, List, Dict, Callable
 from fastapi.applications import FastAPI
 from pydantic import BaseModel
 from fastapi import FastAPI, APIRouter
-from fastapi import Body, FastAPI, HTTPException, Request, Response
+from fastapi import Body, FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
 from fastapi.routing import APIRoute
 
@@ -21,9 +21,11 @@ from starlette.responses import Response
 import traceback
 import sys
 
-
+import time
+import random
+import string
+import traceback
 import pandas as pd
-
 
 class TaskParams(BaseModel):
     session_id : str
@@ -57,61 +59,27 @@ class SessionInfoReq(BaseModel):
 class SaveResp(BaseModel):
     path : str
 
-
-import time
-
-class WorkerState(BaseModel):
-    task_list : List[TaskParams]
-    current_task_index : int
-
 class Worker:
     session_id : str
     task_list : List[TaskParams]
     current_task : int
-    max_accepted : int 
-    max_seen : int 
-    max_time_s : float 
 
-    def __init__(self, session_id, task_list, max_seen = 100, max_accepted = 10, max_time_s = pd.Timedelta('4 minutes').total_seconds()):
+    def __init__(self, session_id, task_list):
         self.session_id = session_id
         self.task_list = task_list
         self.current_task = -1
 
-        self.max_seen = max_seen
-        self.max_accepted = max_accepted
-        self.max_time_s = max_time_s
+    def get_state(self) -> WorkerState:
+        return WorkerState(task_list=self.task_list, current_task_index=self.current_task)
 
-    def get_state(self) ->WorkerState:
-        return WorkerState(task_list=self.task_list, current_task=self.current_task)
-
-    def is_session_done(self, session : Session) -> bool:
-        tot = session.get_totals()
-        start_time = session.action_log[0]['time']
-        time_now = time.time()
-        elapsed = time_now - start_time
-        return tot['seen'] >= self.max_seen  or tot['accepted'] >= self.max_accepted or elapsed >= self.max_time_s
-
-    def next_session_params(self):
+    def next_session(self):
         self.current_task += 1
         task = self.task_list[self.current_task]
         new_params = session_params(**task.dict())
         return new_params
 
-class TaskInfoResp(BaseModel):
-    session_id : str
-    app_state : AppState
-
-import random
-import string
-import traceback
-
 def generate_id():
     return ''.join(random.choice(string.ascii_letters + '0123456789') for _ in range(32))
-
-def prep_db(gdm, index_spec):
-    hdb = gdm.load_index(index_spec.d_name, index_spec.i_name)
-    # TODO: add subsetting here
-    return hdb
 
 from .util import reset_num_cpus
 from .configs import _session_modes, _dataset_map, std_linear_config,std_textual_config
@@ -139,7 +107,6 @@ def session_params(mode, dataset, session_id, **kwargs):
   base.other_params = {'mode':mode, 'dataset':dataset, **kwargs}
   return base
 
-import random
 
 def generate_task_list(mode, session_id):
     tasks = []
@@ -182,146 +149,169 @@ class ErrorLoggingRoute(APIRoute):
 app = FastAPI()
 app.router.route_class = ErrorLoggingRoute
 
+class SingleSeesaw:
+    """ holds the state for a single user state machine. All actions here are serially run.
+        API mirrors the one in WebSeesaw for single user operations
+    """
+    session_id : str
+    session : Optional[Session]
+    worker : Optional[Worker]
+
+    def __init__(self, root_dir, save_path, session_id, worker : Worker = None, num_cpus=None):
+        if num_cpus is not None:
+            reset_num_cpus(num_cpus)
+
+        self.session_id = session_id
+        self.root_dir = root_dir
+        self.save_path = save_path
+        self.worker = worker
+
+        self.gdm = GlobalDataManager(root_dir)
+        self.indices = self.gdm.list_indices()
+        self.session = None
+
+    def _reset_dataset(self,  s: SessionParams):
+        res = make_session(self.gdm, s)
+        self.session = res['session']
+
+    def next_task(self):
+        params = self.worker.next_session()
+        return self._reset_dataset(params)
+
+    def getstate(self):
+        return  AppState(indices=None,
+                        default_params=None,
+                        worker_state=self.worker.get_state() if self.worker else None,
+                        session=self.session.get_state() if self.session else None)
+
+    def reset(self, r : ResetReq):
+        if r.config is not None:
+            self._reset_dataset(r.config)
+        return self.getstate()
+
+    def next(self, body : SessionReq):
+        state = body.client_data.session
+        if state is not None: ## refinement code
+            self.session.update_state(state)
+            self.session.refine()
+        self.session.next()
+        return self.getstate()
+
+    def text(self, key : str):
+        self.session.set_text(key=key)
+        self.session.next()
+        return self.getstate()
+
+    def save(self, body : SessionReq):
+        if body.client_data.session:
+            self.session.update_state(body.client_data.session)
+
+        self.session._log('save')        
+        qkey = self.session.params.other_params['qkey']
+        if qkey not in g_queries:
+            qkey = 'other'
+        
+        output_path = f'{self.save_path}/session_{self.session_id}/qkey_{qkey}/saved_{time.strftime("%Y%m%d-%H%M%S")}'
+        os.makedirs(output_path, exist_ok=False)
+        base = self.getstate().dict()
+        json.dump(base, open(f'{output_path}/summary.json', 'w'))
+        print(f'saved session {output_path}')
+        return SaveResp(path='')
+
 class WebSeesaw:
-    session : Session
-    sessions : Dict[str,Session]
+    sessions : Dict[str,SingleSeesaw]
     def __init__(self, root_dir, save_path, num_cpus=None):
         if num_cpus is not None:
             reset_num_cpus(num_cpus)
 
         self.root_dir = root_dir
         self.save_path = save_path
-
-        self.gdm = GlobalDataManager(root_dir)
-        print('gdm done')
-        self.indices = self.gdm.list_indices()
-        print(self.indices)
-        print('indices done')
-        self.workers = {} # maps worker_id to state
-        self.default_params = _session_modes['textual']
-        self.session = None
-        self.sessions = {}
-
-
-    def _reset_dataset(self,  s: SessionParams):
-        hdb = prep_db(self.gdm, s.index_spec)
-        print('prep db done')
-        res = make_session(self.gdm, s)
-        session = res['session']
-        self.sessions[s.session_id] = session
-        self.session = session
-        print('new session ready')
+        self.num_cpus = num_cpus
+        self.sessions = {} # maps session id to state...
 
     def _create_new_worker(self, mode):
         session_id = generate_id()
-        self.workers[session_id] = Worker(session_id=session_id, task_list=generate_task_list(mode, session_id))
+        worker = Worker(session_id=session_id, task_list=generate_task_list(mode, session_id))
+        self.sessions[session_id] = SingleSeesaw(self.root_dir, self.save_path, session_id, worker, num_cpus=self.num_cpus)
         return session_id
-
-    def _reset_to_next_task(self, session_id):
-        w = self.workers[session_id]
-        new_params = w.next_session_params()
-        self._reset_dataset(new_params)
-
-    def _getstate(self):
-        return AppState(#indices=self.indices, 
-                        #default_params=self.session.params if self.session is not None else self.default_params,
-                        session=self.session.get_state() if self.session is not None else None)
-
-    @app.get('/getstate', response_model=AppState)
-    def getstate(self, session_id):
-        self.session = self.sessions[session_id]
-        return self._getstate()
-
-    @app.post('/reset', response_model=AppState)
-    def reset(self, r : ResetReq):
-        print('reset request', r)
-        if r.config is not None:
-            self._reset_dataset(r.config)
-
-        return self._getstate()
-
-    @app.post('/next', response_model=AppState)
-    def next(self, body : SessionReq):
-        print('/next req for session ', body.session_id)
-        assert body.session_id in self.sessions
-        self.session = self.sessions[body.session_id]
-        state = body.client_data.session
-        if state is not None: ## refinement code
-            self.session.update_state(state)
-            self.session.refine()
-        self.session.next()
-        s =  self._getstate()
-        print('done with /next req')
-        return s
-
-    @app.post('/text', response_model=AppState)
-    def text(self, session_id: str, key : str):
-        assert session_id in self.sessions
-        self.session = self.sessions[session_id]
-        self.session.set_text(key=key)
-        self.session.next()
-        return self._getstate()
-
-    @app.post('/save', response_model=SaveResp)
-    def save(self, body : SessionReq):
-        assert body.session_id in self.sessions
-        self.session = self.sessions[body.session_id]
-        self.session.update_state(body.client_data.session)
-        self.session._log('save')
-        
-        qkey = self.session.params.other_params['qkey']
-        if qkey not in g_queries:
-            qkey = 'other'
-
-        output_path = f'{self.save_path}/session_{body.session_id}/qkey_{qkey}/saved_{time.strftime("%Y%m%d-%H%M%S")}'
-        #os.path.realpath(output_path)
-        os.makedirs(output_path, exist_ok=False)
-        base = self._getstate().dict()
-        json.dump(base, open(f'{output_path}/summary.json', 'w'))
-        print(f'Saved session {output_path}')
-        return SaveResp(path='')
-
-    @app.post('/session_info', response_model=AppState)
-    def session_info(self, body : SessionInfoReq):
-        assert os.path.isdir(body.path)
-        sum_path = f'{body.path}/summary.json'
-        all_info  = json.load(open(sum_path, 'r'))
-        if 'bench_params' in all_info: # saved benchmark
-            return AppState(indices=[], session=all_info['result']['session'], default_params=all_info['result']['session']['params'])
-        else: # saved web session
-            return AppState(indices=all_info['indices'], session=all_info['session'], default_params=all_info['session']['params'])
 
     @app.post('/user_session', response_model=AppState)
     def user_session(self, mode, dataset, session_id, qkey, user):
-        if session_id in self.sessions:
-            print('using existing session')
-            self.session = self.sessions[session_id]
-        else:
+        """ API for the old-school user study where we generated URLs and handed them out.
+        """ 
+        # will make a new session if the id is new
+        if session_id not in self.sessions:
             ## makes a new session using a config for the given mode
             print('start user_session request: ', mode, dataset, session_id)
             new_params = session_params(mode, dataset, session_id, qkey=qkey, user=user)
             print('new user_session params used:', new_params)
-            self._reset_dataset(new_params)
+            new_session = SingleSeesaw(self.root_dir, self.save_path, session_id=session_id, num_cpus=self.num_cpus)
+            new_session._reset_dataset(new_params)
+            self.sessions[session_id] = new_session
 
-        st =  self._getstate()
-        print('completed user_session request')
-        return st
+        return self.sessions[session_id].getstate()
     
     @app.post('/session', response_model=AppState)
-    def session(self, mode, session_id = Cookie(None)):
-        """ assigns a new session id
+    def session(self, mode : str, response : Response, session_id = Cookie(None)):
+        """ creates a new (multi-session) session_id  and sets a session id cookie when none exists.
         """
         if session_id is None:
             session_id = self._create_new_worker(mode)
-            self._reset_to_next_task(session_id)
+            response.set_cookie(key='session_id', value=session_id, max_age=pd.Timedelta('2 hours').total_seconds())
         elif session_id not in self.sessions:
             raise HTTPException(status_code=404, detail=f"unknown {session_id=}")
         else:
             pass
 
-        self.session = self.sessions[session_id]
-        st = self._getstate()
-        if st.session.params.other_params['mode'] != mode:
-            raise HTTPException(status_code=400, detail=f"session {session_id=} already exists with different mode")
+        s = self.sessions[session_id]
+        st = s.getstate()
+        if st.session:
+            if st.session.params.other_params['mode'] != mode:
+                raise HTTPException(status_code=400, detail=f"session {session_id=} already exists with different mode")
+        
         return st
 
+    @app.post('/session_info', response_model=AppState)
+    def session_info(self, body : SessionInfoReq):
+        """ Used for visualizing (read-only) old session logs stored in files
+        """
+        assert os.path.isdir(body.path)
+        sum_path = f'{body.path}/summary.json'
+        all_info  = json.load(open(sum_path, 'r'))
+        if 'bench_params' in all_info: # saved benchmark
+            return AppState(indices=None, worker_state=None,
+                            session=all_info['result']['session'], 
+                            default_params=all_info['result']['session']['params'])
+
+        else: # saved web session
+            return AppState(indices=None, worker_state=None,
+                            session=all_info['session'], 
+                            default_params=all_info['session']['params'])
+
+    
+    """
+        Single-session forwarding functions (forward calls)
+    """
+    @app.get('/getstate', response_model=AppState)
+    def getstate(self, session_id = Cookie(None)):
+        return self.sessions[session_id].getstate()
+
+    @app.post('/reset', response_model=AppState)
+    def reset(self, r : ResetReq, session_id = Cookie(None)):
+        return self.sessions[session_id].reset(r)
+
+    @app.post('/next', response_model=AppState)
+    def next(self, body : SessionReq, session_id = Cookie(None)):
+        return self.sessions[session_id].next(body)
+
+    @app.post('/text', response_model=AppState)
+    def text(self, key : str, session_id = Cookie(None)):
+        return self.sessions[session_id].text(key)
+
+    @app.post('/next_task', response_model=AppState)
+    def next_task(self, session_id = Cookie(None)):
+        return self.sessions[session_id].next_task()
+
+    @app.post('/save', response_model=SaveResp)
+    def save(self, body : SessionReq, session_id = Cookie(None)):
+        return self.sessions[session_id].save(body)
