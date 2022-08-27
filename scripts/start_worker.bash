6d325882448ad307f06ee07914c51c0a4d5cfa22
Oscar Moll <orm@csail.mit.edu>
2022-08-27 15:54:51 -0400

    allow head node to change also

--- scripts/start_worker.bash
+++ scripts/start_worker.bash
@@ -6,24 +6,24 @@ TMPNAME=/state/partition1/user/$USER/raytmp/
 ## different nodes have differnt amounts available.
 ## leave some
 SHM_AVAILABLE_KB=`df /dev/shm | grep -v Available | awk '{print $4}'`
-OBJ_MEM_BYTES=$(( SHM_AVAILABLE_KB*1024 - 1024*1024  )) # for some reason I get very small numbers with this
+OBJ_MEM_BYTES=$(( SHM_AVAILABLE_KB*1024 - 1024*1024  ))
 
 COMMON_ARGS="--temp-dir=$TMPNAME  --object-store-memory=$OBJ_MEM_BYTES --num-cpus=$((2*SLURM_CPUS_ON_NODE))"
 
-SIGFILE="$HOME/init_spc.head_node"
+SIGFILE="$HOME/ray.head"
 
 if [[ $1 == "--head" ]]
 then
     ray start $COMMON_ARGS --head
-
     echo $HOSTNAME > $SIGFILE # signal change after done starting
 else 
-    HEAD_NODE=`cat $SIGFILE`
     PREV=
 
     while true
     do
         CURRENT=`stat -c '%y' $SIGFILE`
+        HEAD_NODE=`cat $SIGFILE`
+
         if [[ $PREV != $CURRENT ]]
         then
             echo 'file changed... restarting node'
@@ -31,7 +31,7 @@ else
             if [[ $USER == omoll ]]; # im using virtual env rather than default evnv.
             then 
                 # copy environment first
-                rsync -rlugvR  $HEAD_NODE:/state/partition1/user/omoll/venvs/seesaw/ /
+                rsync -rlugvR --delete $HEAD_NODE:/state/partition1/user/omoll/venvs/seesaw/ /
         
                 set +x
                 source /state/partition1/user/omoll/venvs/seesaw/bin/activate
@@ -40,7 +40,7 @@ else
             ## just a test that these things work in the current environment
             # python -c 'import torch; import ray; import transformers; import seesaw'
             ray stop
-            ray start $COMMON_ARGS --address=$HEAD_NODE:6379 --redis-password=5241590000000000 
+            ray start $COMMON_ARGS --address=$HEAD_NODE:6379 --redis-password=5241590000000000
         else
             echo 'no change'
         fi
