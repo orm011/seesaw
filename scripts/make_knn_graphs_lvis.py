8ae3dd65b3abadd3f6ea2e3a7c3317bbad087042
Oscar Moll <orm@csail.mit.edu>
2022-10-26 20:32:27 -0400

    exctract knn graphs

--- scripts/make_knn_graphs_lvis.py
+++ scripts/make_knn_graphs_lvis.py
@@ -4,6 +4,7 @@ import os
 import ray
 ray.init('auto', namespace='seesaw')
 from seesaw.dataset_manager import GlobalDataManager
+from seesaw.knn_graph import compute_knn_from_nndescent, compute_exact_knn
 
 root = '/home/gridsan/omoll/fastai_shared/omoll/seesaw_root2/'
 gdm = GlobalDataManager(root)
@@ -17,6 +18,10 @@ import pyroaring as pr
 # os.makedirs(g_output_path, exist_ok=True)
 # import shutil
 
+dataset_name = 'lvis'
+index_name = 'multiscale'
+knng_name = 'nndescent60'
+opts = dict(use_vec_index=False)
 
 from seesaw.util import reset_num_cpus
 
@@ -26,21 +31,19 @@ class KNNMaker:
         self.num_cpus = num_cpus
         gdm = GlobalDataManager(root)
         self.gdm = gdm
-        self.ds = gdm.get_dataset('lvis')
-        self.index = self.ds.load_index('coarse', options=dict())
+        self.ds = gdm.get_dataset(dataset_name)
+        self.index = self.ds.load_index(index_name, options=opts)
         ## brings index out but not used
 
     def __call__(self, batch):
-        print(f'{len(batch)=}')
         for subset_name in batch:
-            print(subset_name)
+            print(f'{subset_name=}')
             sds = self.ds.load_subset(subset_name)
-            idx = sds.load_index('coarse', options=dict())
-            final_path = idx.get_knng_path()
-            knng, _ = KNNGraph.from_vectors(idx.vectors, n_neighbors=30, n_jobs=self.num_cpus, low_memory=True)
-
+            idx = sds.load_index(index_name, options=opts)
+            final_path = idx.get_knng_path(knng_name)
+            df = compute_knn_from_nndescent(idx.vectors, n_neighbors=60, n_jobs=self.num_cpus, low_memory=True)
             os.makedirs(final_path, exist_ok=True)
-            knng.save(final_path, num_blocks=1) 
+            df.to_parquet(f'{final_path}/forward.parquet')
             print('done with ', final_path)
         return batch
 
@@ -48,7 +51,7 @@ class KNNMaker:
 all_subsets = qgt.columns.values
 ds = ray.data.from_items(all_subsets, parallelism=250)
 
-num_cpus = 20
+num_cpus = 21
 fn_args = dict(num_cpus=num_cpus)
 ray_remote_args=dict(num_cpus=num_cpus)
 from ray.data.dataset import ActorPoolStrategy
