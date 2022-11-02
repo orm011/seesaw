0c6ebe4b6b500226e1fb98472ccc84b9edd1ab30
Oscar Moll <orm@csail.mit.edu>
2022-11-02 11:47:56 -0400

    allow plugging in new lr

--- seesaw/research/multiscale_lr_eval.py
+++ seesaw/research/multiscale_lr_eval.py
@@ -68,9 +68,13 @@ def train_test_split_framewise(vec_meta, random_state):
     test_meta = vec_meta[vec_meta.dbidx.isin(set(tst_idcs))]
     return train_meta, test_meta
 
-def eval_multiscale_lr(root, idxname, category, frame_pooling, random_state, **lropts):
+def eval_multiscale_lr(*, root, dataset, idxname, category, frame_pooling, random_state, RegressionClass, **   lropts):
     gdm = GlobalDataManager(root)
-    ds = gdm.get_dataset('lvis').load_subset(category)
+    if dataset == 'lvis':
+        ds = gdm.get_dataset(dataset).load_subset(category)
+    else:
+        ds = gdm.get_dataset(dataset)
+
     idx = ds.load_index(idxname,  options=dict(use_vec_index=False))
 
     boxes, _ = ds.load_ground_truth()
@@ -80,7 +84,7 @@ def eval_multiscale_lr(root, idxname, category, frame_pooling, random_state, **l
     vec_meta = vec_meta.assign(vectors=TensorArray(idx.vectors), ys=(vec_meta.max_iou > 0).astype('float32'))
     train_meta, test_meta = train_test_split_framewise(vec_meta, random_state)
 
-    lr = LogisticRegressionPT(**lropts)
+    lr = RegressionClass(**lropts)
     
     lr.fit(train_meta.vectors.to_numpy(), train_meta.ys.values.reshape(-1,1).astype('float'))
 
