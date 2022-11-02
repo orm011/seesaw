de5d87edf3b2fc0fe952d81c0aec3cbc59bc9e7c
Oscar Moll <orm@csail.mit.edu>
2022-11-02 11:48:49 -0400

    test for max inversions and add a test case

--- seesaw/test_rank_loss.py
+++ seesaw/test_rank_loss.py
@@ -13,6 +13,7 @@ _test_cases = [
          scores = [],
          margin = 10.,
          inversions = [],
+         max_inversions = [],
          rank_loss = [],
          gradient = [],
     ),
@@ -22,6 +23,7 @@ _test_cases = [
          scores = [.5],
          margin = 1.,
          inversions = [0],
+         max_inversions = [0],
          rank_loss = [0.],
          gradient = [0.],
     ),
@@ -31,6 +33,7 @@ _test_cases = [
          scores = [.1, .2],
          margin = 0.,
          inversions = torch.zeros((2,2)),
+         max_inversions = [1., 1.],
          rank_loss = torch.zeros(2),
          gradient = torch.zeros(2),
     ),
@@ -40,6 +43,7 @@ _test_cases = [
          scores = [.09, .2],
          margin = 0.1,
          inversions = torch.zeros((2,2)),
+         max_inversions = [1., 1.],
          rank_loss = torch.zeros(2),
          gradient = torch.zeros(2),
     ),
@@ -49,6 +53,7 @@ _test_cases = [
          scores = [.2, .09],
          margin = 0.1,
          inversions = torch.zeros((2,2)),
+         max_inversions = [1., 1.],
          rank_loss = torch.zeros(2),
          gradient = torch.zeros(2),
     ),
@@ -60,6 +65,9 @@ _test_cases = [
          inversions = [[0, -1], # need to increase score
                         [1., 0]
          ],
+
+          max_inversions = [1., 1.],
+
          rank_loss = [[0, .01], # small loss after taking maring off.01
                       [.01, 0.]
          ],
@@ -74,6 +82,8 @@ _test_cases = [
          inversions = [[0, -1], # need to increase score
                         [1., 0]
          ],
+
+          max_inversions = [1., 1.],
          rank_loss = [[0, .0], # loss is technically zero
                       [.0, 0.]
          ],
@@ -86,6 +96,7 @@ _test_cases = [
          margin = 0.,
          inversions = [[0, -1],   # negative when element is scored lower than it should be
                         [1, 0.]], # positive when element is scored higher than it should be
+          max_inversions = [1., 1.],
          rank_loss = [[0, .1],  
                       [.1, 0]], # rank loss is always positive.
 
@@ -98,6 +109,7 @@ _test_cases = [
          margin = 0.,
          inversions = [[0, 1],   # negative when element is scored lower than it should be
                         [-1, 0.]], # positive when element is scored higher than it should be
+          max_inversions = [1., 1.],
          rank_loss = [[0, .1],  
                       [.1, 0]], # rank loss is always positive.
 
@@ -110,6 +122,7 @@ _test_cases = [
          margin = .11,
          inversions = [[0, -1], 
                         [1, 0.]],
+          max_inversions = [1., 1.],
          rank_loss = [[0, .21],  # loss is bigger
                       [.21, 0]],
          gradient = [1, -1.], # gradient stays the same
@@ -121,16 +134,18 @@ _test_cases = [
          scores = [.1, .2],
          margin = .11,
          inversions = torch.zeros((2,2)),
+         max_inversions = [0., 0.],
          rank_loss = torch.zeros(2),
          gradient = torch.zeros(2),
     ),
 
     # repeated target value different order
     # also no losses
-    dict(target = [1., 1.],
+    dict(target = [2., 2.],
          scores = [.2, .1],
          margin = .11,
          inversions = torch.zeros((2,2)),
+          max_inversions = [0., 0.],
          rank_loss = torch.zeros(2),
          gradient = torch.zeros(2),
     ),
@@ -141,6 +156,7 @@ _test_cases = [
          margin = 0.,
          inversions = [[0, -1], 
                         [1, 0.]],
+          max_inversions = [1., 1,],
          rank_loss = [[0, .0],  # loss value is zero here
                       [.0, 0]],
          gradient = [1, -1.], # despite loss being right at 0, we should make the first score lower, second one higher 
@@ -153,6 +169,7 @@ _test_cases = [
          inversions = [[0,  -1, -1], 
                         [1, 0., -1],
                         [1, 1, 0]],
+          max_inversions = [2., 2., 2.],
          rank_loss = [[0, .1,  .2],  # loss is zero here, but we are at the boundary
                       [.1, .0, .1],
                       [.2, .1, 0.]
@@ -170,6 +187,7 @@ _test_cases = [
                         [0,  1,  0, 0],
                         [0,  0,  0, 0]
                     ],
+          max_inversions = [2., 2., 2., 2],
          rank_loss = [[0,  0,  0, 0],  # loss is zero here, but we are at the boundary
                       [0,  0, .1, 0],
                       [0, .1,  0, 0],
@@ -177,6 +195,27 @@ _test_cases = [
                       ],
 
          gradient = [0, 1., -1., 0.], # second vector has zero gradient, but other two have larger
+    ),
+
+
+        # mixed case with duplicate targets
+    dict(target = [0, 1, 1, 1, 2],
+         scores = [.4,.1,.2,.3, .0],
+         margin = 0.,
+         inversions = [ [0, -1, -1, -1, -1], 
+                        [1,  0,  0,  0, -1],
+                        [1,  0,  0,  0, -1],
+                        [1,  0,  0,  0, -1],
+                        [1,  1,  1,  1,  0]
+                    ],
+         max_inversions = [4., 2., 2., 2, 4],
+         rank_loss = [[0,  .3,  .2, .1,  .4],  # loss is zero here, but we are at the boundary
+                      [.3,  0.,  0., 0., .1],
+                      [.2,  0.,  0., 0., .2],
+                      [.1,  0.,  0., 0., .3],
+                      [.4,  .1,  .2, .3,  0]
+                     ],
+         gradient = [4, 0., 0., 0., -4], # second vector has zero gradient, but other two have larger
     ),
 
     # degenerate score case
@@ -188,6 +227,7 @@ _test_cases = [
                     [1, 0, -1],
                     [1, 1, 0],
             ],
+            max_inversions = [2., 2., 2.,],
             rank_loss = torch.zeros((3,3)),
             gradient = [2, 0, -2],
     )
@@ -219,7 +259,7 @@ def test_ref_pairwise_loss():
 def test_ref_pairwise_rank_loss_gradient():
     for test in get_test_cases():
         computed = ref_pairwise_rank_loss_gradient(test['target'], scores=test['scores'], margin=test['margin'])
-        expected = test['gradient']
+        expected = 2*test['gradient']
         assert torch.isclose(computed, expected).all(), f'{computed=} {expected=}'
 
 
@@ -227,5 +267,12 @@ def test_quick_pairwise_rank_loss_gradient_zero_margin():
     for test in get_test_cases():
         if test['margin'] == 0:
             computed = quick_pairwise_gradient_zero_margin(test['target'], scores=test['scores'])
-            expected = test['gradient']
+            expected = 2*test['gradient']
             assert torch.isclose(computed, expected).all(), f'{computed=} {expected=}'
+
+
+def test_quick_pairwise_rank_loss_max_inversions():
+    for test in get_test_cases():
+          _, computed = quick_pairwise_gradient_zero_margin(test['target'], scores=test['scores'], return_max_inversions=True)
+          expected = test['max_inversions']
+          assert torch.isclose(computed, expected).all(), f'{computed=} {expected=}'
