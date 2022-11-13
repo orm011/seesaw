755b9f701a38970c363d562f351b5a14fe9319d8
Oscar Moll <orm@csail.mit.edu>
2022-11-12 23:36:41 -0500

    larger knn graphs

--- scripts/make_knn_graphs_lvis.py
+++ scripts/make_knn_graphs_lvis.py
@@ -9,13 +9,13 @@ from seesaw.util import reset_num_cpus
 
 root = '/home/gridsan/omoll/fastai_shared/omoll/seesaw_root2/'
 gdm = GlobalDataManager(root)
-# lvisds = gdm.get_dataset('lvis')
+lvisds = gdm.get_dataset('lvis')
 # index = lvisds.load_index('coarse', options=dict())
-# _, qgt = lvisds.load_ground_truth()
+_, qgt = lvisds.load_ground_truth()
 import pyroaring as pr
 
-knng_name = 'nndescent60'
-n_neighbors = 60
+n_neighbors = 120
+knng_name = f'nndescent{n_neighbors}'
 
 def build_and_save_knng(idx, *, knng_name, n_neighbors, num_cpus, low_memory):
     final_path = idx.get_knng_path(knng_name)
@@ -41,21 +41,22 @@ class KNNMaker:
                 ds = ds.load_subset(subset_name)
             index = ds.load_index(index_name, options=dict(use_vec_index=False))
             build_and_save_knng(index, knng_name=knng_name, n_neighbors=n_neighbors, 
-                    num_cpus=self.num_cpus, low_memory=False)
+                    num_cpus=self.num_cpus, low_memory=True)
         return batch
 
 
 combinations = []
 for dataset_name in ('lvis',):
-    for index_name in ('multiscalemed',):
-        combinations.append((dataset_name, index_name, None))
+    for index_name in ('multiscale',):
+        for category in qgt.columns.values:
+            combinations.append((dataset_name, index_name, category))
 
 # all_subsets = qgt.columns.values
-ds = ray.data.from_items(combinations, parallelism=min(len(combinations), 1))
+ds = ray.data.from_items(combinations, parallelism=min(len(combinations), 300))
 
-num_cpus = 90
+num_cpus = 8
 fn_args = dict(num_cpus=num_cpus)
-ray_remote_args=dict(num_cpus=num_cpus, num_gpus=0)
+ray_remote_args=dict(num_cpus=num_cpus, num_gpus=0, memory=5*((1024)**3))
 from ray.data.dataset import ActorPoolStrategy
 
-x = ds.map_batches(KNNMaker, batch_size=1, compute=ActorPoolStrategy(1, 1), fn_constructor_kwargs=fn_args, **ray_remote_args).take_all()
+x = ds.map_batches(KNNMaker, batch_size=1, compute=ActorPoolStrategy(1, 300), fn_constructor_kwargs=fn_args, **ray_remote_args).take_all()
