910c2286674697d3cf355bc094c565c20775fbdd
Oscar Moll <orm@csail.mit.edu>
2023-03-11 14:43:12 -0500

    add balance flag. handle start cases for pairwise policy

--- seesaw/loops/multi_reg.py
+++ seesaw/loops/multi_reg.py
@@ -27,6 +27,7 @@ class RegModule(nn.Module):
          reg_data_lambda, reg_norm_lambda, reg_query_lambda,
          use_qvec_norm,
          rank_loss_margin=0.,
+         pos_weight,
          verbose=False, max_iter=100, lr=1.):
         super().__init__()
         layer = nn.Linear(dim, 1, bias=False)  # use it for initialization           
@@ -45,7 +46,7 @@ class RegModule(nn.Module):
         self.reg_norm_lambda = reg_norm_lambda
         self.reg_data_lambda = reg_data_lambda
 
-        self.pos_weight = torch.tensor([1.])
+        self.pos_weight = pos_weight #'balanced' #torch.tensor([1.])
         self.rank_loss_margin = rank_loss_margin
 
     def get_coeff(self):
@@ -55,34 +56,59 @@ class RegModule(nn.Module):
         return X @ self.weight
     
     def _step(self, batch):
-        if len(batch) == 2:
+        if len(batch) == 2:                
             X,y=batch # note y can be a floating point
-            weight=None
+            if y is None:
+                sample_weight = None
+            else:
+                sample_weight = torch.ones_like(y)
+        elif len(batch) == 3:
+            X,y,sample_weight = batch
         else:
-            assert len(batch) == 3
-            X,y,weight = batch
+            assert False
 
         item_losses = (0. * self.weight).sum()
         if X is not None:            
+            item_losses = torch.zeros_like(sample_weight) 
+
             logits = self(X, y)
+            orig_sum = sample_weight.sum()
+            pos_total = y@sample_weight
+            neg_total = orig_sum - pos_total
             if self.label_loss_type == 'ce_loss':
-                weighted_celoss = F.binary_cross_entropy_with_logits(logits, y, weight=weight,
-                                            reduction='none', pos_weight=self.pos_weight)        
-                item_losses = weighted_celoss
+                if self.pos_weight == 'balanced':
+                    positive_weight = (neg_total+1.)/(pos_total + 1.)
+                elif type(self.pos_weight) is float:
+                    positive_weight = torch.tensor([self.pos_weight])
+                else:
+                    assert False, 'unknown pos weight type'
+                    
+                celoss = F.binary_cross_entropy_with_logits(logits, y, weight=None,
+                                            reduction='none', pos_weight=None)
+
+                sample_weight[y == 1] *= positive_weight
+                sample_weight *= orig_sum/sample_weight.sum()
+
+                assert torch.isclose(sample_weight.sum(), orig_sum)
+                item_losses = celoss
             elif self.label_loss_type == 'pairwise_rank_loss':
-                per_item_loss, max_inv = ref_pairwise_rank_loss(y, scores=logits, 
-                                    aggregate='sum',margin=self.rank_loss_margin, return_max_inversions=True)
-                
-                per_item_normalized = per_item_loss/max_inv
-                item_losses = per_item_normalized
+                if pos_total > 0 and neg_total > 0:
+                    per_item_loss, max_inv = ref_pairwise_rank_loss(y, scores=logits, 
+                                        aggregate='sum',margin=self.rank_loss_margin, return_max_inversions=True)
+                    
+                    per_item_normalized = per_item_loss/max_inv
+                    item_losses = per_item_normalized
             elif self.label_loss_type == 'pairwise_logistic_loss':
-                per_item_loss, max_inv = ref_pairwise_logistic_loss(y, scores=logits, 
-                                    aggregate='sum', return_max_inversions=True)
-                
-                per_item_normalized = per_item_loss/max_inv
-                item_losses = per_item_normalized
+                if pos_total > 0 and neg_total > 0:
+                    per_item_loss, max_inv = ref_pairwise_logistic_loss(y, scores=logits, 
+                                        aggregate='sum', return_max_inversions=True)
+                    
+                    per_item_normalized = per_item_loss/max_inv
+                    item_losses = per_item_normalized 
             else:
                 assert False
+
+            item_losses *= sample_weight
         
         nweight = F.normalize(self.weight, dim=-1)
 
@@ -93,15 +119,16 @@ class RegModule(nn.Module):
             loss_norm = self.weight.norm()**2
             loss_queryreg = loss_norm*0.
 
-        loss_datareg = nweight @ (self.xlx_matrix @ nweight)
         loss_labels = item_losses.sum()
-        total_loss =  loss_labels + self.reg_data_lambda*loss_datareg + self.reg_norm_lambda*loss_norm + self.reg_query_lambda*loss_queryreg
-
+        loss_datareg = self.reg_data_lambda*(nweight @ (self.xlx_matrix @ nweight))
+        loss_norm = self.reg_norm_lambda*loss_norm
+        loss_queryreg = self.reg_query_lambda*loss_queryreg
+        total_loss =  loss_labels + loss_datareg + loss_norm + loss_queryreg
         return {
-            'loss_norm' :  self.reg_norm_lambda*loss_norm,
+            'loss_norm' : loss_norm,
             'loss_labels': loss_labels,
-            'loss_datareg': self.reg_data_lambda*loss_datareg,
-            'loss_queryreg': self.reg_query_lambda*loss_queryreg,
+            'loss_datareg': loss_datareg,
+            'loss_queryreg': loss_queryreg,
             'loss': total_loss,
         }
     
@@ -118,11 +145,17 @@ class RegModule(nn.Module):
 
     def fit(self, X, y, matchdf):
         trainer_ = BasicTrainer(mod=self, max_epochs=1, verbose=self.verbose)
+        
+        ## 1/(nvecs for image)
+
+        vecs_per_image = matchdf.groupby('dbidx').size().rename('num_vecs').reset_index()
+        matchdf = pd.merge(matchdf, vecs_per_image, left_on='dbidx', right_on='dbidx')
+        vec_weight = matchdf.num_vecs.astype('float').pow(-1).values
 
         if X.shape[0] > 0:
             Xmu = X.mean(axis=0)
             X = X - Xmu.reshape(1,-1) # center this
-            ds = TensorDataset(torch.from_numpy(X), torch.from_numpy(y))
+            ds = TensorDataset(torch.from_numpy(X), torch.from_numpy(y), torch.from_numpy(vec_weight))
             dl = DataLoader(ds, batch_size=X.shape[0], shuffle=True)
         else:
             dl= None
@@ -156,6 +189,8 @@ class MultiReg(PointBased):
         X = self.q.index.vectors[matchdf.index.values]
         y = matchdf.ys.values
 
+        #if self.options['random_as_negative']:
+
         assert self.curr_qvec is not None
 
         model_ = RegModule(dim=X.shape[1], xlx_matrix=self.xlx_matrix, qvec=torch.from_numpy(self.curr_qvec).float(), 
@@ -167,6 +202,7 @@ class MultiReg(PointBased):
                             reg_query_lambda=self.options['reg_query_lambda'], 
                             verbose=self.options['verbose'], 
                             max_iter=self.options['max_iter'], 
+                            pos_weight=self.options['pos_weight'],
                             lr=self.options['lr'])
 
         model_.fit(X, y, matchdf)
