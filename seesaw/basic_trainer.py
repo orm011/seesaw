04ccb21ba50efd6becdc28945b9a80e813859c1f
Oscar Moll <orm@csail.mit.edu>
2023-03-12 18:25:37 -0400

    debug assertions

--- seesaw/basic_trainer.py
+++ seesaw/basic_trainer.py
@@ -11,43 +11,60 @@ class BasicTrainer:
     def fit(self, train_loader):
         self.mod.train()
 
-        batch = (None,None,)
-        batch_idx = None
+        batch = (None,None)
+        batch_idx = -1
+
+        weight = self.opt._params[0]
+        assert not weight.isnan().any(), f'{weight=} {batch_idx=}'
+
 
         loss_history = []
-        def closure():
-            self.opt.zero_grad()
-            ret = self.mod.training_step(batch, batch_idx)            
-            weight = self.opt._params[0]
-            prev_grad = torch.zeros_like(weight)
+        with torch.autograd.set_detect_anomaly(True):
 
-            if self.verbose:
-                entry = []
-                for (k,v) in ret.items():
-                    if k == 'loss':
-                        continue
-                    
-                    v.backward(retain_graph=True)
-                    grad_change = (weight.grad - prev_grad)
-                    entry.append({'k':k, 'loss':v.detach().item(), 'grad_norm':grad_change.norm().item()})
-                    prev_grad = weight.grad.clone()
+            def closure():
+                # print('called')
+                assert not weight.isnan().any(), f'{weight=} {batch_idx=}'
 
-                loss_history.extend(entry)
                 self.opt.zero_grad()
+                ret = self.mod.training_step(batch, batch_idx)            
 
-            ret['loss'].backward()
+                prev_grad = torch.zeros_like(weight)
 
-            if self.verbose:
-                assert torch.isclose(weight.grad, prev_grad, atol=1e-5).all(), f'{weight.grad.norm()=} {prev_grad.norm()=}'
+                if self.verbose:
+                    entry = []
+                    for (k,v) in ret.items():
+                        if k == 'loss':
+                            continue
+                        
+                        v.backward(retain_graph=True)
+                        assert not weight.isnan().any(), f'{weight=} {weight.grad=} {batch_idx=} {k=} {v=} '
 
-            return ret['loss']
+                        grad_change = (weight.grad - prev_grad)
+                        entry.append({'k':k, 'loss':v.detach().item(), 'grad_norm':grad_change.norm().item()})
+                        prev_grad = weight.grad.clone()
 
-        for _ in range(self.max_epochs):
-            if train_loader is not None:
-                for batch_idx, batch in enumerate(train_loader):
+                    loss_history.extend(entry)
+                    self.opt.zero_grad()
+
+                ret['loss'].backward()
+                #print(f'{weight=} {batch_idx=} {ret=} {weight.grad=}')
+                loss_history.append({'k':'total_loss', 'loss':ret['loss'].detach().item(), 'grad_norm':weight.grad.norm().item()})
+                assert not weight.isnan().any(), f'{weight=} {batch_idx=} {ret=} {weight.grad=}'
+
+                if self.verbose:
+                    assert torch.isclose(weight.grad, prev_grad, atol=1e-5).all(), f'{weight.grad.norm()=} {prev_grad.norm()=}'
+
+                return ret['loss']
+
+            for _ in range(self.max_epochs):
+                if train_loader is not None:
+                    for batch_idx, batch in enumerate(train_loader):
+                        self.opt.step(closure)
+                        assert not weight.isnan().any(), f'{weight=} {batch_idx=} {loss_history[-5:]=}'
+                else:
                     self.opt.step(closure)
-            else:
-                self.opt.step(closure)
+                    assert not weight.isnan().any(), f'{weight=} {batch_idx=} {loss_history[-5:]=}'
+
 
         return loss_history
 
