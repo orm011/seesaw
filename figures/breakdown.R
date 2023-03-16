d5063a01ba6b0641468415eafc14dd36146eb60e
Oscar Moll <orm@csail.mit.edu>
2023-03-16 17:15:36 -0400

    updated figure

--- figures/breakdown.R
+++ figures/breakdown.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(arrow)
 
 
-target = c(3.8,1.5)
+target = c(3.8,2.)
 units = 'in'
 
 if (all(near(dev.size(units=units), target))){
@@ -18,42 +18,44 @@ sz <- 3
 update_geom_defaults("text", list(size = sz))
 update_geom_defaults("label", list(size = sz))
 
-table <- read_parquet('./breakdown.parquet', as_data_frame=TRUE)
+table <- read_parquet('./breakdown_df.parquet', as_data_frame=TRUE)
 table <- tibble(table)
 
 table <- (table %>% mutate(dataset=factor(dataset, c('objectnet', 'lvis', 'coco', 'bdd'))))
 
-table <- (table %>% mutate(version=recode_factor(version, baseline='CLIP embedding', 
-                                                 '+ multi-scale representation'='+ multi-scale',  
-                                                 '+ CLIP-guided vector alignment'='+ CLIP alignment', 
-                                                 '+ DB matched training'='+ DB matching')))
+# table <- (table %>% mutate(version=recode_factor(version, baseline='CLIP embedding', 
+#                                                  '+ multi-scale representation'='+ multi-scale',  
+#                                                  '+ CLIP-guided vector alignment'='+ CLIP alignment', 
+#                                                  '+ DB matched training'='+ DB matching')))
 
 
-tableagg <- (table %>% group_by(gp, version) %>% summarise(AP=mean(AP)) %>% mutate(dataset='all'))
+tableagg <- (table %>% group_by(query_group, table_row, dataset) %>% summarise(average_precision=mean(average_precision))) 
+#tableagg <- 
+globalagg <- tableagg %>% group_by(query_group, table_row) %>% summarise(average_precision=mean(average_precision))
 
-table <- (bind_rows(table, tableagg))
-table <- (table %>% filter(gp=='all queries'))
+table <- bind_rows(tableagg, globalagg %>% mutate(dataset='avg'))
+              
+table <- (table %>% mutate(dataset=factor(dataset, c('avg', 'objectnet', 'lvis','bdd', 'coco')), 
+          AP=average_precision))
 
-table <- (table %>% mutate(dataset=factor(dataset, c('all', 'objectnet', 'lvis','bdd', 'coco'))))
-
-bases <- (table %>% group_by(dataset) %>% summarise(AP=min(AP)))
+bases <- (table %>% group_by(query_group, dataset) %>% summarise(AP=min(AP)))
 
 ## add baseline to table, column by group
-table <- merge(table, bases, by.x = 'dataset', by.y ='dataset', suffixes = c('', '_base'))
+table <- merge(x = table, bases, by.x = c('query_group', 'dataset'), by.y =c('query_group', 'dataset'), suffixes = c('', '_base'))
 
-table <- (table %>% mutate(dataset=recode_factor(dataset, all='all', objectnet='ObjNet', lvis='LVIS', coco='COCO', bdd='BDD')))
+table <- (table %>% mutate(dataset=recode_factor(dataset, avg='avg', objectnet='ObjNet', lvis='LVIS', coco='COCO', bdd='BDD')))
 
 value_offset = .5
 
 plot <- (ggplot(data=table)  + 
-          geom_col(aes(x=version, y=(AP - AP_base*.99)*2), alpha=0.)
-          + geom_col(aes(x=version, y=(AP - AP_base*.99)), width=1, color='black')
+          geom_col(aes(x=table_row, y=(AP - AP_base*.99)*2), alpha=0.)
+          + geom_col(aes(x=table_row, y=(AP - AP_base*.99)), width=1, color='black')
           + scale_x_discrete(limits=rev)
           # + ylim(.5,1.)
           + coord_flip()
-          + facet_grid(cols=vars(dataset), scales = 'free_x')
+          + facet_grid(cols=vars(dataset), rows=vars(query_group), scales = 'free')
           # + facet_grid(cols=vars(dataset), rows=vars(gp)) 
-          + geom_text(aes(x=version, y=(AP - AP_base*.99)*1.05,  
+          + geom_text(aes(x=table_row, y=(AP - AP_base*.99)*1.05,  
                           label=str_remove(sprintf("%0.2f", round(AP, digits = 2)), "^0+")),
                       hjust='left', color='black')
           + theme(axis.title.x = element_blank(), 
@@ -64,13 +66,12 @@ plot <- (ggplot(data=table)  +
                   axis.title.y = element_blank(),
                   panel.spacing.x = unit(0, units = 'mm'),
                   panel.background = element_blank(),
-                  plot.margin=grid::unit(c(0,0,0,0), "mm"),
-                  panel.spacing.y = unit(0, units='mm'),
-                  strip.background = element_rect(color='white')
+                  #panel.border = element_rect(color = "black", fill = NA, size = .1),
+                  #plot.margin=grid::unit(c(0,0,0,0), "mm"),
+                  panel.margin=unit(.05, 'lines'),
+                  panel.spacing.y = unit(1, units='mm'),
+                  strip.background = element_rect(color='gray', size = .1)
          ))
 
-
-
-
-ggsave(plot = plot, filename='./breakdown.pdf',  bg = NULL)
+ggsave(plot = plot, filename='./paper_figures/breakdown.pdf',  bg = NULL)
 plot
